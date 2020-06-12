7490c494df562fb245123cd7c51d4d133fc57bdf
유현지 <skykk99@naver.com>
2020-06-13 08:54:21 +0900

    Modify StoreStylingServer

--- 5th_Seminar(todayhouse)/5th_Seminar(todayhouse)/Resources/APIServices/StoreReviewData.swift
+++ 5th_Seminar(todayhouse)/5th_Seminar(todayhouse)/Resources/APIServices/StoreReviewData.swift
@@ -15,65 +15,49 @@ struct StoreReviewData: Codable {
     var message: String
     var data: [Data]?
     
-    enum CodingKeys: String, CodingKey {
-        case status = "status"
-        case success = "success"
-        case message = "message"
-        case data = "data"
-    }
+    struct Data: Codable {
+        var sellIdx : Int
+        var category : String
+        var company : String
+        var title : String
+        var price : Int
+        var discount : Int
+        var imgs : [Imgs]?
+        var reviews : [Reviews]?
     
-    init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        status = (try? values.decode(Int.self, forKey: .status )) ?? -1
-        success = (try? values.decode(Bool.self, forKey: .success )) ?? false
-        message = (try? values.decode(String.self, forKey: .message )) ?? ""
-        data = (try? values.decode([Data].self, forKey: .data )) ?? []
+        enum CodingKeys: String, CodingKey {
+            case sellIdx = "sellIdx"
+            case category = "category"
+            case company = "company"
+            case title = "title"
+            case price = "price"
+            case discount = "discount"
+            case imgs = "imgs"
+            case reviews = "reviews"
+        }
+        
+        init(from decoder: Decoder) throws {
+            let values = try decoder.container(keyedBy: CodingKeys.self)
+            sellIdx = (try? values.decode(Int.self, forKey: .sellIdx )) ?? -1
+            category = (try? values.decode(String.self, forKey: .category )) ?? ""
+            company = (try? values.decode(String.self, forKey: .company )) ?? ""
+            title = (try? values.decode(String.self, forKey: .title )) ?? ""
+            price = (try? values.decode(Int.self, forKey: .price )) ?? -1
+            discount = (try? values.decode(Int.self, forKey: .discount )) ?? -1
+            imgs = (try? values.decode([Imgs].self, forKey: .imgs)) ?? []
+            reviews = (try? values.decode([Reviews].self, forKey: .reviews)) ?? []
+        }
+        
+        struct Imgs: Codable {
+            var imgUrl : String
+        }
+
+        struct Reviews: Codable {
+            var imgUrl : String
+            var profileImgUrl : String
+            var name : String
+            var createdAt : String
+            var content : String
+        }
     }
 }
-
-struct Data: Codable {
-    var sellIdx : Int
-    var category : String
-    var company : String
-    var title : String
-    var price : Int
-    var discount : Int
-    var imgs : [Imgs]?
-    var reviews : [Reviews]?
-    
-    enum CodingKeys: String, CodingKey {
-        case sellIdx = "sellIdx"
-        case category = "category"
-        case company = "company"
-        case title = "title"
-        case price = "price"
-        case discount = "discount"
-        case imgs = "imgs"
-        case reviews = "reviews"
-    }
-    
-    init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        sellIdx = (try? values.decode(Int.self, forKey: .sellIdx )) ?? -1
-        category = (try? values.decode(String.self, forKey: .category )) ?? ""
-        company = (try? values.decode(String.self, forKey: .company )) ?? ""
-        title = (try? values.decode(String.self, forKey: .title )) ?? ""
-        price = (try? values.decode(Int.self, forKey: .price )) ?? -1
-        discount = (try? values.decode(Int.self, forKey: .discount )) ?? -1
-        imgs = (try? values.decode([Imgs].self, forKey: .imgs)) ?? []
-        reviews = (try? values.decode([Reviews].self, forKey: .reviews)) ?? []
-    }
-}
-
-struct Imgs: Codable {
-    var imgUrl : String
-}
-
-struct Reviews: Codable {
-    var imgUrl : String
-    var profileImgUrl : String
-    var name : String
-    var createdAt : String
-    var content : String
-}
-
--- 5th_Seminar(todayhouse)/5th_Seminar(todayhouse)/Sources/Cell/StoreStylingShotTableViewCell.swift
+++ 5th_Seminar(todayhouse)/5th_Seminar(todayhouse)/Sources/Cell/StoreStylingShotTableViewCell.swift
@@ -15,111 +15,119 @@ class StoreStylingShotTableViewCell: UITableViewCell {
     @IBOutlet weak var stylingbigImg: UIImageView!
     
     //Cell에 들어갈 데이터저장배열
-    var stylingdataset : StoreReviewData!
-    var stylingdataList : [Data] = []
-    var stylingimgList : [Imgs] = []
-    var stylingreviewList : [Reviews] = []
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
+        //var stylingdataset : [Data] = []
+        //var stylingdataset : StoreReviewData!
+        var stylingdataList : [StoreReviewData.Data] = []
+        var stylingimgList : [StoreReviewData.Data.Imgs] = []
+        var stylingreviewList : [StoreReviewData.Data.Reviews] = []
         
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.scrollDirection = .horizontal
-        flowLayout.itemSize = CGSize(width: 77, height: 77)
-        flowLayout.minimumLineSpacing = 15.0
-        flowLayout.minimumInteritemSpacing = 8.0
-        flowLayout.sectionInset.left = 15.0
-        
-        self.StylingshotCV.collectionViewLayout = flowLayout
-        self.StylingshotCV.showsHorizontalScrollIndicator = false
-        
-        // Register the xib for collection view cell
-        let cellNib = UINib(nibName: "StylingshotCell", bundle: nil)
-        self.StylingshotCV.register(cellNib, forCellWithReuseIdentifier: "stylingshotCell")
-        
-        //통신 부분
-        SellService.shared.getSell(){
-            responsedata in
-            switch responsedata {
-            case .success(let data):
-                
-                self.stylingdataset = data as? StoreReviewData
-                if let StylingdataList = data as? [Data]{
-                    self.stylingdataList = StylingdataList
+        override func awakeFromNib() {
+            super.awakeFromNib()
+            
+            let flowLayout = UICollectionViewFlowLayout()
+            flowLayout.scrollDirection = .horizontal
+            flowLayout.itemSize = CGSize(width: 77, height: 77)
+            flowLayout.minimumLineSpacing = 15.0
+            flowLayout.minimumInteritemSpacing = 8.0
+            flowLayout.sectionInset.left = 15.0
+            
+            self.StylingshotCV.collectionViewLayout = flowLayout
+            self.StylingshotCV.showsHorizontalScrollIndicator = false
+            
+            // Register the xib for collection view cell
+            let cellNib = UINib(nibName: "StylingshotCell", bundle: nil)
+            self.StylingshotCV.register(cellNib, forCellWithReuseIdentifier: "stylingshotCell")
+            
+            //통신 부분
+            SellService.shared.getSell(){
+                responsedata in
+                switch responsedata {
+                case .success(let data):
+                    /*
+                    self.stylingdataset = (data as? [Data])!
+                    print(self.stylingdataset)
+                    */
+                    if let StylingdataList = data as? [StoreReviewData.Data]{
+                        self.stylingdataList = StylingdataList
+                        print(self.stylingdataList)
+                        print(self.stylingimgList)
+                        print(self.stylingreviewList)
+                    }
+                    else if let StylingimgList = data as? [StoreReviewData.Data.Imgs]{
+                        self.stylingimgList = StylingimgList
+                        //print(self.stylingimgList)
+                    }
+                    else if let StylingreviewList = data as? [StoreReviewData.Data.Reviews]{
+                        self.stylingreviewList = StylingreviewList
+                        //print(self.stylingreviewList)
+                    }
+                    self.StylingshotCV.dataSource = self
+                    
+                    print("success")
+                case .requestErr(_):
+                    print("request error")
+                case .pathErr:
+                    print("pathErr")
+                case .serverErr:
+                    print("serverErr")
+                case .networkFail :
+                    print("failure")
                 }
-                else if let StylingimgList = data as? [Imgs]{
-                    self.stylingimgList = StylingimgList
-                }
-                else if let StylingreviewList = data as? [Reviews]{
-                    self.stylingreviewList = StylingreviewList
-                }
-                self.StylingshotCV.dataSource = self
-                
-                print("success")
-            case .requestErr(_):
-                print("request error")
-            case .pathErr:
-                print("pathErr")
-            case .serverErr:
-                print("serverErr")
-            case .networkFail :
-                print("failure")
             }
-        }
-        
-        // Comment if you set Datasource and delegate in .xib
-        //self.StylingshotCV.dataSource = self
-        self.StylingshotCV.delegate = self
-        
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
-    
-}
-
-extension StoreStylingShotTableViewCell: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        print(stylingdataList.count)
-        print(stylingimgList.count)
-        print(stylingreviewList.count)
-        return stylingimgList.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if let cell = StylingshotCV.dequeueReusableCell(withReuseIdentifier: "stylingshotCell", for: indexPath) as? StylingshotCell {
             
-            let reviewsImg = stylingimgList[indexPath.row]
-            
-            cell.stylingpriviewImg.imageFromUrl(reviewsImg.imgUrl, defaultImgPath: "http:// ~~ ")
-            //cell.stylingpriviewImg.image = UIImage(named: stylingpriviewList[indexPath.row].stylingpriviewImg)
-            return cell
+            // Comment if you set Datasource and delegate in .xib
+            //self.StylingshotCV.dataSource = self
+            self.StylingshotCV.delegate = self
             
         }
-        return UICollectionViewCell()
+
+        override func setSelected(_ selected: Bool, animated: Bool) {
+            super.setSelected(selected, animated: animated)
+
+            // Configure the view for the selected state
+        }
+        
+        
     }
-    
-}
 
-extension StoreStylingShotTableViewCell: UICollectionViewDelegate {
-}
+    extension StoreStylingShotTableViewCell: UICollectionViewDataSource {
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            print(stylingdataList.count)
+            print(stylingimgList.count)
+            print(stylingreviewList.count)
+            return stylingimgList.count
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            if let cell = StylingshotCV.dequeueReusableCell(withReuseIdentifier: "stylingshotCell", for: indexPath) as? StylingshotCell {
+                
+                let reviewsImg = stylingimgList[indexPath.row]
+                
+                cell.stylingpriviewImg.imageFromUrl(reviewsImg.imgUrl, defaultImgPath: "http:// ~~ ")
+                //cell.stylingpriviewImg.image = UIImage(named: stylingpriviewList[indexPath.row].stylingpriviewImg)
+                return cell
+                
+            }
+            return UICollectionViewCell()
+        }
+        
+    }
 
-//Kingfisher로 이미지 url 받아오는 구문
-extension UIImageView {
-    public func imageFromUrl(_ urlString: String?, defaultImgPath : String) {
-        let defaultImg = UIImage(named: defaultImgPath)
-        if let url = urlString {
-            if url.isEmpty {
-                self.image = defaultImg
+    extension StoreStylingShotTableViewCell: UICollectionViewDelegate {
+    }
+
+    //Kingfisher로 이미지 url 받아오는 구문
+    extension UIImageView {
+        public func imageFromUrl(_ urlString: String?, defaultImgPath : String) {
+            let defaultImg = UIImage(named: defaultImgPath)
+            if let url = urlString {
+                if url.isEmpty {
+                    self.image = defaultImg
+                } else {
+                    self.kf.setImage(with: URL(string: url), placeholder: defaultImg, options: [.transition(ImageTransition.fade(0.5))])
+                }
             } else {
-                self.kf.setImage(with: URL(string: url), placeholder: defaultImg, options: [.transition(ImageTransition.fade(0.5))])
+                self.image = defaultImg
             }
-        } else {
-            self.image = defaultImg
         }
     }
-}
