39fef913a99b6e59b2ba868bc6111d7aedbbf1ae
jaeukyang <didwodnr123@gmail.com>
2020-06-12 22:47:05 +0900

    서버 통신 어렵다..

--- 5th_Seminar(todayhouse)/5th_Seminar(todayhouse)/Resources/APIServices/PicviewService.swift
+++ 5th_Seminar(todayhouse)/5th_Seminar(todayhouse)/Resources/APIServices/PicviewService.swift
@@ -12,31 +12,5 @@ import Alamofire
 struct PicviewService {
     static let shared = PicviewService()
     
-    func getRoomData(completion: @escaping (NetworkResult<Any>) -> Void) {
-        let header: HTTPHeaders = ["Content-Type": "application/json"]
-        let dataRequest = Alamofire.request(APIConstants.postURL, method: .get, encoding: JSONEncoding.default, headers: header)
-        dataRequest.responseData { dataResponse in
-            switch dataResponse.result {
-            case .success:
-                guard let statusCode = dataResponse.response?.statusCode else { return }
-                guard let value = dataResponse.result.value else { return }
-                let networkResult = self.judge(by: statusCode, value)
-                completion(networkResult)
-            case .failure: completion(.networkFail)
-            }
-        }
-    }
-    private func judge(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
-        switch statusCode {
-        case 200: return isUser(by: data)
-        case 400: return .pathErr
-        case 500: return .serverErr default: return .networkFail
-        }
-    }
-    private func isUser(by data: Data) -> NetworkResult<Any> {
-        let decoder = JSONDecoder()
-        guard let decodedData = try? decoder.decode(PicviewData.self, from: data) else { return .pathErr }
-        guard let tokenData = decodedData.data else { return .requestErr(decodedData.message) }
-        return .success(tokenData.jwt)
-    }
+    
 }
