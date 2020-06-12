90e1803f16c278e30aaf87b526274303db2d25ba
jaeukyang <didwodnr123@gmail.com>
2020-06-12 23:18:24 +0900

    json 파싱

--- 5th_Seminar(todayhouse)/5th_Seminar(todayhouse)/Resources/APIServices/PicviewService.swift
+++ 5th_Seminar(todayhouse)/5th_Seminar(todayhouse)/Resources/APIServices/PicviewService.swift
@@ -8,33 +8,31 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 struct PicviewService {
+    private init(){}
     static let shared = PicviewService()
     
-    func getData(completion: @escaping (NetworkResult<Any>) -> Void) {
-        let header: HTTPHeaders = ["Content-Type": "application/json"]
-        let dataRequest = Alamofire.request(APIConstants.postURL, method: .get, encoding: JSONEncoding.default, headers: header)
-        dataRequest.responseData { dataResponse in
-            switch dataResponse.result {
-            case .success:
-                guard let statusCode = dataResponse.response?.statusCode else { return }
-                guard let value = dataResponse.result.value else { return }
-            let networkResult = self.judge(by: statusCode, value)
-            completion(networkResult)
-            case .failure: completion(.networkFail)
+    let header: HTTPHeaders = ["Content-Type": "application/json"]
+    
+    func getData(completion: @escaping (_ b: Bool) -> Void) {
+        
+        Alamofire.request(APIConstants.postURL).responseJSON{ response in
+            switch response.result{
+            case .success(let value):
+                let json = JSON(value)
+                print(json)
+                let result = json["status"].intValue
+                if result == 404{
+                    completion(true)
+                }
+                else{
+                    completion(false)
+                }
+            default:
+                return
             }
         }
-    }
-    private func judge(by statusCode: Int, _ data: Data) -> NetworkResult<Any> { switch statusCode {
-        case 200: return isUser(by: data)
-        case 400: return .pathErr
-        case 500: return .serverErr default: return .networkFail }
-    }
-    private func isUser(by data: Data) -> NetworkResult<Any> {
-        let decoder = JSONDecoder()
-        guard let decodedData = try? decoder.decode(PicviewData.self, from: data) else { return .pathErr }
-        guard let detailData = decodedData.data else { return .requestErr(decodedData.message) }
-        return .success(DetailData)
     }
 }
